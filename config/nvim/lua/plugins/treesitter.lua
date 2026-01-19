c7ad6433e13f1a24606289e17ac7c3af70f3c925
miyabisun <miyabi.ooh@gmail.com>
2026-01-19 14:21:17 +0900

    fix(nvim): nvim-treesitter

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,10 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-      local configs = require("nvim-treesitter.config")
+      local status, configs = pcall(require, "nvim-treesitter.configs")
+      if not status then
+        configs = require("nvim-treesitter.config")
+      end
 
       configs.setup({
         ensure_installed = {
